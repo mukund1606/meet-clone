6f466e8576de8856df3983720107846b248bac30
Mukund Mittal <mukund.mukulmittal@gmail.com>
2024-08-07 04:56:08 +0530

    Backend Dockerfile Size Reduced, Minor Package Changes, Get Route Created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
 let workers: Worker[] = [];
 export let nextMediasoupWorkerIdx = 0;
 
